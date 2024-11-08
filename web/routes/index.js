4a2568abddcc7b9eb7b6ef62e478d8d823208afc
Victor <vicaji05@gmail.com>
2024-11-08 11:45:14 +0100

    mensaje

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var data = require("../data/dataprovider");
 
-/* GET home page. */
+/* Pagina galeria */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  const posts = data.getAllPosts();
+  const categorias = data.getCategories();
+  res.render('galeria', {title:"Galeria", posts:posts, categorias:categorias});
+});
+
+router.get('/entrada/:id', function(req,res){
+  /* RECOGER EL PRODUCTO CON EL ID Y ENVIARLO con req.params.id --> producto = data.getElementById(req.params.id)*/
+  const id = req.params.id;
+  const post = data.getPostById(req.params.id);
+  const categorias = data.getCategories();
+  res.render('individual',{title:"Publicación", post:post, categorias:categorias, indice:id});
 });
 
 module.exports = router;
